a2e1b5e3c487855e8d014caa117ec12f14bc6bc6
Aaron Beyer <aarbeyer@gmail.com>
2024-05-09 10:33:54 -0400

    Update query_3.sql
    Adding actors history scd table

---
+++ submission/query_3.sql
@@ -0,0 +1,14 @@
+CREATE TABLE adbeyer.actors_history_scd (
+  actor_id VARCHAR,
+  actor_name VARCHAR,
+  quality_class VARCHAR,
+  is_active BOOLEAN,
+  start_date INTEGER,
+  end_date INTEGER,
+  current_year INTEGER --adding current year to be able to quickly filter new data
+)
+WITH (
+    format = 'PARQUET',
+    partitioning = ARRAY['current_year']
+
+)
