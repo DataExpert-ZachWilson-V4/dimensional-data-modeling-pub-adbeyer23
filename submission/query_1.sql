6d2dd09584de1293c85c20f2d401abd87393df3c
Aaron Beyer <aarbeyer@gmail.com>
2024-05-15 20:20:45 -0400

    Update query_1.sql
    add year to films array

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -2,7 +2,7 @@ CREATE or REPLACE table adbeyer.actors  (
 	"actor"  VARCHAR,
 	"actor_id" VARCHAR,
 	"films" ARRAY(
-	Row(film VARCHAR, votes INT, rating DOUBLE, film_id VARCHAR) --creating an array for cumulative table design
+	Row(year INT, film VARCHAR, votes INT, rating DOUBLE, film_id VARCHAR) --creating an array for cumulative table design
 	),
 	"quality_class" VARCHAR, --- either star,good, average, bad
 	"is_active" BOOLEAN,
