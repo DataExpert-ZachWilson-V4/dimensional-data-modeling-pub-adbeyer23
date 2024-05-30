ba2fea11fd99604420fd02f19c619d558e12a04c
Aaron Beyer <aarbeyer@gmail.com>
2024-05-30 10:30:28 -0400

    Update query_5.sql
    Fix query

--- submission/query_5.sql
+++ submission/query_5.sql
@@ -1,5 +1,5 @@
-INSERT INTO adbeyer.actors_history_scd --incrementally updates an scd table
-WITH --combining the previous year scd with the current year's data
+INSERT INTO adbeyer.actors_history_scd
+WITH -- combining the previous year SCD with the current year's data
   last_year_scd AS (
     SELECT
       *
@@ -10,35 +10,38 @@ WITH --combining the previous year scd with the current year's data
   ),
   current_year AS (
     SELECT
-      *
+*
     FROM
       adbeyer.actors
     WHERE
       current_year = 1920
   ),
-  combined AS ( --combine last year and current year
+  combined AS ( -- combine last year and current year
     SELECT
-      COALESCE(ly.actor_id, cy.actor_id) AS actor_id,
+      COALESCE(ly.actor, cy.actor_id) AS actor_id,
       COALESCE(ly.start_date, cy.current_year) AS start_year,
-      COALESCE(ly.start_date, cy.current_year) AS end_year,
-      CASE --seeing if active changed
+      COALESCE(ly.end_date, cy.current_year) AS end_year,
+      -- did active change?
+      CASE
         WHEN COALESCE(ly.is_active, FALSE) <> cy.is_active THEN 1
         WHEN COALESCE(ly.is_active, FALSE) = cy.is_active THEN 0
       END AS active_change,
       -- active for this and last year 
       COALESCE(ly.is_active, FALSE) AS is_active_last_year,
       cy.is_active AS is_active_this_year,
-      CASE --seeing if quality class changed
+      -- did quality_class change?
+      CASE
         WHEN COALESCE(ly.quality_class, '') <> cy.quality_class THEN 1
         WHEN COALESCE(ly.quality_class, '') = cy.quality_class THEN 0
       END AS qc_change,
       -- quality class values for this and last year
       COALESCE(ly.quality_class, '') AS qc_last_year,
       cy.quality_class AS qc_this_year,
-      1920 AS current_year  -- current year
+      -- current year
+      1920 AS current_year
     FROM
       last_year_scd AS ly
-      FULL OUTER JOIN current_year AS cy ON ly.actor_id = cy.actor_id
+      FULL OUTER JOIN current_year AS cy ON ly.actor= cy.actor_id
       AND ly.end_date + 1 = cy.current_year
   ),
   results AS (
@@ -46,7 +49,7 @@ WITH --combining the previous year scd with the current year's data
       actor_id,
       current_year,
       CASE
-      -- no changes
+        -- no changes
         WHEN active_change = 0
         AND qc_change = 0 THEN ARRAY[
           CAST(
@@ -56,10 +59,10 @@ WITH --combining the previous year scd with the current year's data
               start_year,
               end_year + 1
             ) AS ROW(
-              is_active boolean,
+              is_active BOOLEAN,
               qc VARCHAR,
-              start_year integer,
-              end_year integer
+              start_year INTEGER,
+              end_year INTEGER
             )
           )
         ]
@@ -73,10 +76,10 @@ WITH --combining the previous year scd with the current year's data
               start_year,
               end_year
             ) AS ROW(
-              is_active boolean,
+              is_active BOOLEAN,
               qc VARCHAR,
-              start_year integer,
-              end_year integer
+              start_year INTEGER,
+              end_year INTEGER
             )
           ),
           CAST(
@@ -86,13 +89,14 @@ WITH --combining the previous year scd with the current year's data
               current_year,
               current_year
             ) AS ROW(
-              is_active boolean,
+              is_active BOOLEAN,
               qc VARCHAR,
-              start_year integer,
-              end_year integer
+              start_year INTEGER,
+              end_year INTEGER
             )
           )
-        ] --when there are new records
+        ]
+        -- when there are new records
         WHEN active_change IS NULL
         OR qc_change IS NULL THEN ARRAY[
           CAST(
@@ -102,10 +106,10 @@ WITH --combining the previous year scd with the current year's data
               start_year,
               end_year
             ) AS ROW(
-              is_active boolean,
+              is_active BOOLEAN,
               qc VARCHAR,
-              start_year integer,
-              end_year integer
+              start_year INTEGER,
+              end_year INTEGER
             )
           )
         ]
@@ -113,7 +117,7 @@ WITH --combining the previous year scd with the current year's data
     FROM
       combined
   )
-SELECT  --unpacking the array to produce the results
+SELECT
   results.actor_id,
   arr.qc,
   arr.is_active,
